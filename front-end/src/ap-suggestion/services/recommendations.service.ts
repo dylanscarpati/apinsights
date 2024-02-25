13e10d510f840a0d33326b8c4a41ff4d001cd3a9
dylanscarpati <dylanscarpati@gmail.com>
2024-02-25 01:54:33 -0500

    Implemented Angular Services for AP Classes, Majors, and Recommendations
    Enhanced the frontend functionality by implementing services that interact with the NestJS backend. This involved creating three key services within the Angular services folder, each responsible for fetching and submitting data related to AP classes, majors, and recommendations.

--- front-end/src/ap-suggestion/services/recommendations.service.ts
+++ front-end/src/ap-suggestion/services/recommendations.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ApClass } from '../../models/ap-class.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecommendationsService {
+  private apiUrl = `${environment.apiUrl}/recommendations`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getRecommendations(major1: string, major2: string): Observable<ApClass[]> {
+    // Construct the query parameters
+    const queryParams = `?major1=${encodeURIComponent(major1)}&major2=${encodeURIComponent(major2)}`;
+
+    return this.http.get<ApClass[]>(`${this.apiUrl}${queryParams}`);
+  }
 }
