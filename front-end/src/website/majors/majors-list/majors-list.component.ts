12b01c6b0777fe354c0e44382900f018d1d22814
dylanscarpati <dylanscarpati@gmail.com>
2024-02-29 22:50:50 -0500

    Finalized Majors Page
    - grey out the buttons when two majors are selected
    - fix error where button doesn't change color after deselected

--- front-end/src/website/majors/majors-list/majors-list.component.ts
+++ front-end/src/website/majors/majors-list/majors-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 import { MajorsService, Major } from '../../../services/majors.service';
-import { NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-majors-list',
@@ -12,7 +11,10 @@ export class MajorsListComponent implements OnInit {
   majors: Major[] = [];
   selectedMajors: Major[] = [];
 
-  constructor(private majorsService: MajorsService, private router: Router) {}
+  constructor(
+    private majorsService: MajorsService, 
+    private router: Router, 
+  ) {}
 
   ngOnInit(): void {
     this.majors = this.majorsService.findAll();
@@ -22,17 +24,24 @@ export class MajorsListComponent implements OnInit {
     const index = this.selectedMajors.findIndex(m => m.id === major.id);
   
     if (index >= 0) {
-      this.selectedMajors.splice(index, 1);
+      this.selectedMajors = this.selectedMajors.filter((_, i) => i !== index);
     } else {
       if (this.selectedMajors.length < 2) {
-        this.selectedMajors.push(major);
+        this.selectedMajors = [...this.selectedMajors, major];
       }
     }
   }
-  
 
   isSelected(major: Major): boolean {
     return this.selectedMajors.some(m => m.id === major.id);
+  }
+  
+  isSelectionComplete(): boolean {
+    return this.selectedMajors.length === 2;
+  }
+
+  shouldDisableButton(major: Major): boolean {
+    return this.selectedMajors.length === 2 && !this.isSelected(major);
   }
 
   generateRecommendations(): void {
@@ -46,5 +55,4 @@ export class MajorsListComponent implements OnInit {
       this.router.navigate(['/recommendations'], queryParams);
     }
   }  
-  
 }
